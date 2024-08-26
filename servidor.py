5aca3250cae74dfa0aada8a67b6f1f36474da850
Isaac Braga <31108956+isaacbragaa@users.noreply.github.com>
2024-08-26 17:08:26 -0300

    Update servidor.py

--- servidor.py
+++ servidor.py
@@ -2,11 +2,9 @@ import socket
 import struct
 import threading
 
-# Configurações do servidor
 HOST = 'localhost'
 PORT = 12345
 
-# Para garantir que o nonce só seja aceito uma vez
 nonce_encontrado = None
 lock = threading.Lock()
 
@@ -14,25 +12,22 @@ def handle_client(conn, addr):
     global nonce_encontrado
     print(f'Cliente conectado: {addr}')
 
-    # Envia uma mensagem inicial para o cliente
     conn.sendall(b'Pronto para receber o nonce.')
 
-    # Recebe o nonce do cliente
     data = conn.recv(1024)
-    nonce = struct.unpack('i', data)[0]  # Desserializa o nonce recebido
+    nonce = struct.unpack('i', data)[0]
 
     with lock:
-        # Verifica se o nonce já foi encontrado
-       if nonce_encontrado is not None:
-            conn.sendall('Nonce já encontrado por outro cliente.'.encode('utf-8'))
-       else:
+        if nonce_encontrado is not None:
+            conn.sendall('Nonce já encontrado.'.encode('utf-8'))
+        else:
             nonce_encontrado = nonce
             conn.sendall(b'Nonce aceito!')
 
     print(f'Cliente {addr} enviou nonce: {nonce}')
     conn.close()
 
-def start_server():
+if __name__ == '__main__':
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.bind((HOST, PORT))
         s.listen()
@@ -41,6 +36,3 @@ def start_server():
         while True:
             conn, addr = s.accept()
             threading.Thread(target=handle_client, args=(conn, addr)).start()
-
-if __name__ == '__main__':
-    start_server()
